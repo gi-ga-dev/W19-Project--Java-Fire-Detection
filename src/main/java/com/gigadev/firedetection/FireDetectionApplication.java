422d6b89f7dad8f70a776b9d3f00e55a1ea1c035
gi-ga-dev <cloud.gianluca@gmail.com>
2022-09-20 15:45:27 +0200

    first commit

---
+++ src/main/java/com/gigadev/firedetection/FireDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.gigadev.firedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigadev/firedetection/FireDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigadev.firedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
